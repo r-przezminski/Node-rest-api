ee947a04a77022611d987d971cd75744aa7c0dde
r-przezminski <r.przezminski@gmail.com>
2018-02-14 19:27:01 +0100

    added cors middleware

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(morgan('dev'));
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+	);
+	if(req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
+		return res.status(200).json({});
+	}
+	next();
+});
+
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
 	res.json({
