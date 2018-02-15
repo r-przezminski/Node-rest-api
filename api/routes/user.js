4f4b30ea7015f5177cdad88c390a899a0f3a40ac
r-przezminski <r.przezminski@gmail.com>
2018-02-15 15:52:49 +0100

    added user login route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controllers/users');
+const userController = require("../controllers/users");
 
-router.post('/register', userController.userRegister);
+router.post("/register", userController.userRegister);
+router.post("/log-in", userController.userLogin);
 
 module.exports = router;
