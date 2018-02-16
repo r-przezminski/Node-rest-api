5c6339d13cd0517e672242aa6f68c47eb91fba39
r-przezminski <r.przezminski@gmail.com>
2018-02-16 17:23:15 +0100

    replace cors middleware to own file

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
+const corsHeaders = require('./api/middleware/cors');
+
 const app = express();
 
+// START MONGODB
+//"C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe"
 mongoose.connect('mongodb://localhost/Node-rest-api');
 mongoose.Promise = global.Promise;
 
@@ -13,17 +17,14 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(corsHeaders);
+
+app.use('/api/user', require('./api/routes/user'));
+
 app.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
-		'Access-Control-Allow-Headers',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	);
-	if(req.method === 'OPTIONS') {
-		res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT, PATCH');
-		return res.status(200).json({});
-	}
-	next();
+	const err = new Error("Not found");
+	err.status = 404;
+	next(err);
 });
 
 app.use((err, req, res, next) => {
