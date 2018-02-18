f8312e5d64b43ad39309d2962e2fbe6a1b1e14e1
r-przezminski <r.przezminski@gmail.com>
2018-02-18 19:16:27 +0100

    added user account route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const verifyToken = require('../middleware/verifyToken');
 const userController = require("../controllers/users");
 
 router.post("/register", userController.userRegister);
 router.post("/log-in", userController.userLogin);
+router.get('/account', verifyToken, userController.userAccount);
 
 module.exports = router;
