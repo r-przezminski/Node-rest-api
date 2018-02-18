b48296372385aff459d1f101201b4420339b3a5b
r-przezminski <r.przezminski@gmail.com>
2018-02-18 19:29:20 +0100

    change user model

--- api/models/user.js
+++ api/models/user.js
@@ -2,22 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-	_id: mongoose.Types.ObjectId,
 	email: {
 		type: String,
-		required: [true, 'email field is required']
+		required: [true, 'Email address is required.'],
+		validate: {
+			validator: v => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v),
+			message: '{VALUE} is not valid email address.',
+		}
 	},
 	password: {
 		type: String,
-		required: [true, 'password field is required']
+		required: [true, 'Password field is required.']
 	},
 	firstName: String,
 	lastName: String,
-	address: [{city: String, street: String, zipCode: String}],
+	address: [{city: String}, {street: String}, {zipCode: String}],
 	phoneNumber: Number,
 	avatar: String,
 	nick: String,
 });
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
