9cac6db77a9081f10a346c15bf3d15a778d54c2f
r-przezminski <r.przezminski@gmail.com>
2018-03-04 22:27:22 +0100

    implement verifying jwt with  passport-jwt strategy and user auth with passport-locale strategy

---
+++ api/middleware/passport.js
@@ -0,0 +1,36 @@
+const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt');
+const { Strategy: LocalStrategy } = require('passport-local');
+
+const User = require('../models/user');
+
+module.exports = passport => {
+  passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_KEY,
+  }, async (payload, done) => {
+    try {
+      const user = await User.findById(payload.id);
+      if (!user) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
+    }
+  }));
+
+  passport.use(new LocalStrategy({
+    usernameField: 'email',
+  }, async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email });
+      const matchedPassword = await user.verifyPassword(password);
+      if (!user && !matchedPassword) {
+        return done(null, false);
+      }
+      return done(null, user);
+    } catch (error) {
+      return done(error, false);
+    }
+  }));
+};
