6bd102f40c836f4c19bdef91a8800b41d02a7e7f
r-przezminski <r.przezminski@gmail.com>
2018-03-04 22:28:10 +0100

    implement verifying jwt with  passport-jwt strategy and user auth with passport-locale strategy

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,87 +1,51 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-exports.userRegister = (req, res, next) => {
-	User.find({email: req.body.email}).exec()
-	.then(user => {
-		if(user.length >= 1) {
-			return res.status(409).json({
-				message: "This email address allready exist."
-			});
-		} else {
-			if(req.body.password !== req.body.repeatPassword) {
-				return res.status(500).json({
-					message: "Paswords are not the same."
-				});
-			}
-			bcrypt.hash(req.body.password, 10, (err, hash) => {
-				if(err) {
-					return res.status(500).json({
-						error: err
-					});
-				}
-				const user = new User({
-					email: req.body.email,
-					password: hash
-				});
-				user.save()
-					.then(result => {
-						return res.status(201).json({
-							message: "Your account has been created, check your email to activate account",
-							user: user
-						});
-					})
-					.catch(err => {
-						return res.status(500).json({
-							message: err
-						})
-					});
-			})
-		}
-	});
+const generateToken = user => {
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+  const secret = process.env.JWT_KEY;
+  const exp = { expiresIn: '1h' };
+  return jwt.sign(payload, secret, exp);
 };
-exports.userLogin = (req, res, next) => {
-	User.findOne({email: req.body.email}).exec()
-		.then(user => {
-			if(!user) {
-				return res.status(401).json({
-					message: "Incorrect email or password"
-				});
-			} 
-			const hash = user.password;
-			bcrypt.compare(req.body.password, hash, (err, result) => {
-				if(err || !result) {
-					return res.status(401).json({
-						message: "Incorrect email or password"
-					});
-				}
-				const token = jwt.sign({ email: user.email, _id: user._id }, process.env.JWT_KEY, { expiresIn: "1h" });
-				return res.status(200).json({
-					message: "Logged in",
-					token: token
-				});
-			});
-		})
-		.catch(err => {
-			return res.status(401).json({
-				message: "Incorrect email or password"
-			})
-		})
+
+module.exports = {
+  userRegister: async (req, res, next) => {
+    try {
+      const user = await User.find({ email: req.body.email });
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: 'This email address already exist',
+        });
+      }
+      if (req.body.password !== req.body.repeatPassword) {
+        return res.status(500).json({
+          message: 'Paswords are not the same.',
+        });
+      }
+      const newUser = new User(req.body);
+      await newUser.save();
+      const token = await generateToken(newUser);
+      return res.status(201).json({
+        message: 'Your account has been created, check your email to activate account',
+        token,
+      });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  userLogin: async (req, res, next) => {
+    try {
+      const token = await generateToken(req.user);
+      return res.status(201).json({ token });
+    } catch (error) {
+      return next(error);
+    }
+  },
+
+  userAccount: async (req, res) => res.status(200).json(req.user),
 };
-exports.userAccount = (req, res, next) => {
-	User.findById(req.userData._id).exec()
-		.then(user => {
-			res.status(200).json({
-				user: user
-			})
-		})
-		.catch(err => {
-			res.status(500).json({
-				error: err
-			})
-		})
-};
-exports.userEdit = (req, res, next) => {}
